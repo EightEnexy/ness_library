45ec648c09f865d34911c8f79ea928b1fcf24718
EightEnexy <enexyges@gmail.com>
2022-09-14 16:47:15 +0200

    suppress warnings

--- ness_lib_project/src/ness/lib/Subscriber.java
+++ ness_lib_project/src/ness/lib/Subscriber.java
@@ -7,6 +7,7 @@ public class Subscriber {
     String email;
     Integer age;
 
+    @SuppressWarnings("unused")
     public Subscriber(String firstName, String lastName, SubscriberAddress address, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -22,43 +23,43 @@ public class Subscriber {
         this.email = email;
         this.age = age;
     }
-
+    @SuppressWarnings("unused")
     public String getFirstName() {
         return firstName;
     }
-
+    @SuppressWarnings("unused")
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
+    @SuppressWarnings("unused")
     public String getLastName() {
         return lastName;
     }
-
+    @SuppressWarnings("unused")
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
+    @SuppressWarnings("unused")
     public SubscriberAddress getAddress() {
         return address;
     }
-
+    @SuppressWarnings("unused")
     public void setAddress(SubscriberAddress address) {
         this.address = address;
     }
-
+    @SuppressWarnings("unused")
     public String getEmail() {
         return email;
     }
-
+    @SuppressWarnings("unused")
     public void setEmail(String email) {
         this.email = email;
     }
-
+    @SuppressWarnings("unused")
     public int getAge() {
         return age;
     }
-
+    @SuppressWarnings("unused")
     public void setAge(int age) {
         this.age = age;
     }
